d7605597f317e042183fc0939560ea1af297ea00
daynju <fakhruddinmu22@gmail.com>
2026-05-26 10:27:55 +0800

    initial portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>My Portfolio</h1>
+      <p>Hi, I'm a Web Developer.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", background: "#333", color: "white" }}>
+      <h2>My Portfolio</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
